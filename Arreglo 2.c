811649556c93ab637ff5fe6d1139122d9e913da0
Axel Frederick Félix Jiménez <72401600+fr3derick117@users.noreply.github.com>
2020-11-23 17:38:06 -0600

    Create Arreglo 2.c

---
+++ Arreglo 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char** argv) {
+int a;
+int b;
+int c;
+scanf("%d", &a);
+int arreglo[a];
+for(int b=0;b<a;b++){
+  scanf("%d", &arreglo[b]);
+ }
+  for(int c=0;c<x;c++){
+ printf("%d\n", arreglo[c]);
+ }
+return (0);
+}
